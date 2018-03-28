dc6772d3c9b6317e97e839210df96616249ce5d4
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-03-28 00:55:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const request = require("request");
+var result="";
+//var app = express();
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+      extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+restService.post("/echo", function(req, res) {
+    var speech =
+      req.body.result &&
+      req.body.result.parameters &&
+      req.body.result.parameters.echoText
+        ? req.body.result.parameters.echoText
+        : "wrong";
+
+    
+    var adr = 'https://mdcs0014121431trial.hanatrial.ondemand.com/ChatBotProject/services/demo7.xsjs';
+    request.get({ url: "https://mdcs0014121431trial.hanatrial.ondemand.com/ChatBotProject/services/demo7.xsjs"}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            result= res.json(body);
+        }
+        else{
+            result="No data";
+        }
+    });
+
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: "wms"
+    });
+});
+
+
