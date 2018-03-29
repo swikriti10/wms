ca3f54057edf4b5855aaf9065a37420084edfb45
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-03-29 02:30:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
+const request = require("request");
 var result="";
 //var app = express();
 const restService = express();
-
+//var speech = "";
 restService.use(
   bodyParser.urlencoded({
       extended: true
@@ -15,21 +15,30 @@ restService.use(
 
 restService.use(bodyParser.json());
 restService.post("/echo", function(req, res) {
-    var speech =
+  var  speech =
       req.body.result &&
       req.body.result.parameters &&
       req.body.result.parameters.echoText
         ? req.body.result.parameters.echoText
         : "wrong";
 
+  request.get({ url: "https://mdcs0014121431trial.hanatrial.ondemand.com/ChatBotProject/services/demo7.xsjs" }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          result = res.json(body);
+      }
+      else {
+          result = "No data";
+      }
+  });
 
     return res.json({
-        speech: speech,
-        displayText: speech,
+        speech: result,
+        displayText: result,
         source: "wms"
     });
 });
-restService.listen(process.env.PORT || 1337, function() {
-  console.log("Server up and listening");
+restService.listen(process.env.PORT || 1337, function () {
+    console.log("running server");
 });
+
 
