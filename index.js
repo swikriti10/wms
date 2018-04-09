e2cd14bda2dd99fdbffb9a610c865348bcfe977f
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-09 06:24:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -132,23 +132,39 @@ restService.post("/video", function(req, res) {
 });
 
 restService.post("/slack-test", function(req, res) {
-    function basicCard () {
-        const app = new ActionsSdkApp({request:req, response:res});
-        app.ask(app.buildRichResponse()
-          // Create a basic card and add it to the rich response
-          .addSimpleResponse('Math and prime numbers it is!')
-          .addBasicCard(app.buildBasicCard('42 is an even composite number. It' +
-            'is composed of three distinct prime numbers multiplied together. It' +
-            'has a total of eight divisors. 42 is an abundant number, because the' +
-            'sum of its proper divisors 54 is greater than itself. To count from' +
-            '1 to 42 would take you about twenty-one…')
-            .setTitle('Math & prime numbers')
-            .addButton('Read more', 'https://example.google.com/mathandprimes')
-            .setImage('https://example.google.com/42.png', 'Image alternate text')
-            .setImageDisplay('CROPPED')
-          )
-        );
+    restService.post("/slack-test", function(req, res) {
+    var slack_message = {
+    
+        google: {
+            expect_user_response: true,
+            rich_response: {
+                items: [
+                  {
+                      simple_response: {
+                          text_to_speech: "This might be the first response"
+                      }
+                  },
+                  {
+                      basic_card: {
+                          image: {
+                              url: "http://rathankalluri.com/tr-in/agents/tr-1024.jpg",
+                              accessibility_text: "Travel India text"
+                          }
+                      }
+                  }
+                ]
+            }
+        }
     }
+
+    return res.json({
+        speech: "speech",
+        displayText: "speech",
+        source: "webhook-echo-sample",
+        data: {
+            google: slack_message
+        }
+    });
 });
 
 restService.listen(process.env.PORT || 8000, function() {
