8a4e37993c3b83fc70b5d217b7c0eea6e44a759b
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-09 07:14:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,112 +13,7 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
 
-restService.post("/audio", function(req, res) {
-  var speech = "";
-  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language 
-    case "music one":
-      speech =
-        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music two":
-      speech =
-        '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music three":
-      speech =
-        '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music four":
-      speech =
-        '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music five":
-      speech =
-        '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>';
-      break;
-    case "delay":
-      speech =
-        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
-      break;
-    //https://www.w3.org/TR/speech-synthesis/#S3.2.3
-    case "cardinal":
-      speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
-      break;
-    case "ordinal":
-      speech =
-        '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
-      break;
-    case "characters":
-      speech =
-        '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
-      break;
-    case "fraction":
-      speech =
-        '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
-      break;
-    case "bleep":
-      speech =
-        '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
-      break;
-    case "unit":
-      speech =
-        '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
-      break;
-    case "verbatim":
-      speech =
-        '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
-      break;
-    case "date one":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
-      break;
-    case "date two":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
-      break;
-    case "date three":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
-      break;
-    case "time":
-      speech =
-        '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
-      break;
-    case "telephone one":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
-      break;
-    case "telephone two":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
-      break;
-    // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
-    case "alternate":
-      speech =
-        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
-      break;
-  }
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
 
 restService.post("/video", function(req, res) {
   return res.json({
@@ -131,44 +26,44 @@ restService.post("/video", function(req, res) {
 });
 
   restService.post("/slack-test", function(req, res) {
-    var x={
-  "google": {
-  "expect_user_response": true,
-  "rich_response": {
-  "items": [
+    var slack_message={
+  
+  expect_user_response: true,
+  rich_response: {
+  items: [
     {
-      "simpleResponse": {
-          "textToSpeech":"This is the first simple response for a basic card"
+      simpleResponse: {
+          textToSpeech:"This is the first simple response for a basic card"
       }
     },
     {
-      "basicCard": {
-        "title":"Title: this is a title",
-        "formattedText":"This is a basic card.  Text in a\n      basic card can include \"quotes\" and most other unicode characters\n      including emoji 📱.  Basic cards also support some markdown\n      formatting like *emphasis* or _italics_, **strong** or __bold__,\n      and ***bold itallic*** or ___strong emphasis___ as well as other things\n      like line  \nbreaks",
-        "subtitle":
+      basicCard: {
+        title:"Title: this is a title",
+        formattedText:"This is a basic card.  Text in a\n      basic card can include \"quotes\" and most other unicode characters\n      including emoji 📱.  Basic cards also support some markdown\n      formatting like *emphasis* or _italics_, **strong** or __bold__,\n      and ***bold itallic*** or ___strong emphasis___ as well as other things\n      like line  \nbreaks",
+        subtitle:
         "This is a subtitle",
-        "image": {
-          "url":"https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-          "accessibilityText":"Image alternate text"
+        image: {
+          url:"https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+          accessibilityText:"Image alternate text"
         },
-        "buttons": [
+        buttons: [
           {
-            "title":"This is a button",
-            "openUrlAction":{
-              "url":"https://assistant.google.com/"
+            title:"This is a button",
+            openUrlAction:{
+              url:"https://assistant.google.com/"
             }
           }
         ]
       }
     },
     {
-      "simpleResponse": {
-        "textToSpeech":"This is the 2nd simple response ",
-        "displayText":"This is the 2nd simple response"
+      simpleResponse: {
+        textToSpeech:"This is the 2nd simple response ",
+        displayText:"This is the 2nd simple response"
       }
     }
   ],
-  "suggestions":
+  suggestions:
   [
     {"title":"Basic Card"},
     {"title":"List"},
@@ -176,18 +71,19 @@ restService.post("/video", function(req, res) {
     {"title":"Suggestions"}
   ]
 }
-}
+
     };
     return res.json({
     speech: "",
-    displayText: "speech",
+    displayText: "",
     source: "webhook-echo-sample",
-    data:x
+    data: {
+      slack: slack_message
+    }
   });
 });
 
 
-});
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
