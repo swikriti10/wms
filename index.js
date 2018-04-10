ccd249a5607117a84998214850585f35aeee9fd8
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-10 02:36:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,88 +2,71 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
+
 const restService = express();
 
 restService.use(
   bodyParser.urlencoded({
-      extended: true
+    extended: true
   })
 );
 
 restService.use(bodyParser.json());
 
-restService.post("/slack-test", function (req, res) {
 
-    const url = `http://services.odata.org/V3/Northwind/Northwind.svc/Customers?$format=json`;
-    
-        var slack_message = {
 
-            expect_user_response: true,
-            rich_response: {
-                items: [
-                  {
-                      simpleResponse: {
-                          textToSpeech: "This is the first simple response for a basic card"
-                      }
-                  },
-                  {
-                      basicCard: {
-                          title: "hi",
-                          formattedText: "hi",
-                          subtitle: "hi",
+restService.post("/video", function(req, res) {
+  return res.json({
+    speech:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    displayText:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    source: "webhook-echo-sample"
+  });
+});
 
-                          image: {
-                              url: "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-                              accessibilityText: "Image alternate text"
-                          },
-                          buttons: [
-                            {
-                                title: "This is a button",
-                                openUrlAction: {
-                                    url: "https://assistant.google.com/"
-                                }
-                            }
-                          ]
-                      }
-                  },
-                  {
-                      simpleResponse: {
-                          textToSpeech: "This is the 2nd simple response ",
-                          displayText: "This is the 2nd simple response"
-                      }
-                  }
-                ],
-                suggestions:
-                [
-                  { "title": "Basic Card" },
-                  { "title": "List" },
-                  { "title": "Carousel" },
-                  { "title": "Suggestions" }
-                ]
-            }
-
-        };
-
-        return res.json({
-            speech: "",
-            displayText: "",
-            source: "webhook-echo-sample",
-            data: {
-                google: slack_message
-            }
-        });
-
-    
-    
+  restService.post("/slack-test", function(req, res) {
+    var slack_message={
+  
+  expect_user_response: true,
+  richResponse: {
+           items: [
+				{
+				    simpleResponse: {
+				        textToSpeech:"This is a simple response for a list"
+				    }
+				}
+            ],
+            suggestions: [
+				{
+				    title:"List"
+				},
+				{
+				    title:"Carousel"
+				},
+				{
+				    title:"Suggestions"
+				}
+            ]
+        }
+        
+        
+    };
+    return res.json({
+    speech: "",
+    displayText: "",
+    source: "webhook-echo-sample",
+    data: {
+      google: slack_message
+    }
+  });
 });
 
 
 
-restService.listen(process.env.PORT || 8000, function () {
-    console.log("Server up and listening");
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
 });
-
 
 
 
