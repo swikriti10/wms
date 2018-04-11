46d7ff27ed4c26cbb4e164d0aad80173d1688d36
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-11 05:10:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,34 +19,52 @@ restService.post("/slack-test", function (req, res) {
 
         var slack_message = {
 
-            expect_user_response: true,
-            rich_response: {
-                items: [
-                  {
-                      simpleResponse: {
-                          textToSpeech:"test"
-                      }
-                  },
-               {
-      basicCard: {
-        title:"Title: this is a title",
-        formattedText:"This is a basic card.  Text in a\n      basic card can include \"quotes\" and most other unicode characters\n      including emoji ??.  Basic cards also support some markdown\n      formatting like *emphasis* or _italics_, **strong** or __bold__,\n      and ***bold itallic*** or ___strong emphasis___ as well as other things\n      like line  \nbreaks",
-        subtitle:
-        "This is a subtitle",
-        image: {
-          url:"https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-          accessibilityText:"Image alternate text"
-        },
-        buttons: [
-          {
-            title:"This is a button",
-            openUrlAction:{
-              url:"https://assistant.google.com/"
-            }
-          }
-        ]
-      }
-    },
+        expect_user_response: true,
+        rich_response: {
+            items: [
+              {
+                  simpleResponse: {
+                      textToSpeech: "This is the first simple response for a basic card"
+                  }
+              },
+              {
+                  listSelect: {
+                      title: "Things to learn about",
+                      items: [
+                          {
+                              "optionInfo": {
+                                  "key": "MATH_AND_PRIME",
+                                  "synonyms": [
+                                      "math",
+                                      "math and prime",
+                                      "prime numbers",
+                                      "prime"
+                                  ]
+                              },
+                              "title": "Math & prime numbers",
+                              "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
+                              "image": {
+                                  "url": "http://example.com/math_and_prime.jpg",
+                                  "accessibilityText": "Math & prime numbers"
+                              }
+                          }
+                             
+
+                      ]
+                  }
+              }
+              ],
+                      suggestions:
+                      [
+                        { "title": "Basic Card" },
+                        { "title": "List" },
+                        { "title": "Carousel" },
+                        { "title": "Suggestions" }
+                      ]
+                  }
+
+              };
+
                  
                   {
                       simpleResponse: {
