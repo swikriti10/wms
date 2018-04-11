864f8d6abc77bef93a75dfda97793022ec693a64
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-11 14:04:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,34 +28,23 @@ restService.post("/slack-test", function (req, res) {
                   }
               },
               {
-                  carouselBrowse: {
+                  list_card: {
                      title: "Awesome List",
                       items: [
-                       {
-          description: "at price of Rs. 57,999",
-          title: "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra) MQD32HN/A A1466",
-          footer: "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra) MQD32HN/A A1466",
-          image: {
-            url: "https://rukminim1.flixcart.com/image/200/200/j4irlow0/computer/j/8/c/apple-na-notebook-original-imaevdrcvuksg2zv.jpeg?q=90",
-            accessibilityText: "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra) MQD32HN/A A1466"
-          },
-          openUrlAction: {
-            url: "https://dl.flipkart.com/dl/apple-macbook-air-core-i5-5th-gen-8-gb-128-gb-ssd-mac-os-sierra-mqd32hn-a/p/itmevcpqqhf6azn3?pid=COMEVCPQBXBDFJ8C&affid=HotDeals20&affExtParam2=pricee-desktop-search-21"
-          }
-        },
-        
-                       {
-          description: "at price of Rs. 89,990",
-          title: "Apple Macbook PRO MPXQ2/R2 Core i5 (6th Gen)/8 GB/128 GB/33.78 cm (13.3)/Mac OS)",
-          footer: "Apple Macbook PRO MPXQ2/R2 Core i5 (6th Gen)/8 GB/128 GB/33.78 cm (13.3)/Mac OS)",
-          image: {
-            url: "https://assetscdn.paytm.com/images/catalog/product/L/LA/LAPAPPLE-MACBOOROSE73954D5B64792/1.jpg",
-            accessibilityText: "Apple Macbook PRO MPXQ2/R2 Core i5 (6th Gen)/8 GB/128 GB/33.78 cm (13.3)/Mac OS)"
-          },
-          openUrlAction: {
-            url: "https://paytmmall.com/apple-macbook-pro-mpxq2-r2-core-i5-6th-gen-8-gb-128-gb-33-78-cm-13-3-mac-os-CMPLXLAPAPPLE-MACBOODUMM202563C836CCA-pdp?product_id=145129487&discoverability=online&src=grid&utm_source=NDTV&utm_medium=affiliate&utm_campaign=NDTV-recharge&utm_term=Gadget360"
-          }
-        }
+                     {
+              optionInfo: {
+                key: "One",
+                synonyms: []
+              },
+              title: "Item one"
+            },
+            {
+              optionInfo: {
+                key: "Two",
+                synonyms: []
+              },
+              title: "Item Two"
+            }
           ]
                        }
                
