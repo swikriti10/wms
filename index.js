3d6a7a08374e6f2332d748e6f8c8b7b8a28eb35f
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-12 02:12:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,31 @@ restService.use(bodyParser.json());
 
 restService.post("/slack-test", function (req, res) {
 
+  var myObj = [
+   {
+       'CustomerID': "ALFKI",
+       'CompanyName': "Alfreds Futterkiste",
+       'ContactName': "Maria Anders"
 
+
+   },
+   {
+       'CustomerID': "ANATR",
+       'CompanyName': "Ana Trujillo Emparedados y helados",
+       'ContactName': "Ana Trujillo"
+
+   }];
+   
+    for (i = 0; i < myObj.length; i++) {
+
+        var tmp = {
+            'optionInfo': { 'key': myObj[i].CustomerID },
+            'title': myObj[i].CompanyName,
+            'description': myObj[i].ContactName
+        };
+
+        obj.push(tmp);
+    }
     var slack_message = {
 
         expect_user_response: true,
@@ -49,42 +73,7 @@ restService.post("/slack-test", function (req, res) {
                 "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
                 listSelect: {
                     title: "List Title", 
-		    items: [
-						{
-						    optionInfo: {
-						        key: "title",
-						        synonyms: [
-								"synonym of title 1",
-								"synonym of title 2",
-								"synonym of title 3"
-						        ]
-						    },
-						    title: "Title of First ListItem",
-						    
-						},
-						{
-						    optionInfo: {
-						        key: "googleHome",
-						        synonyms: [
-									"Google Home Assistant", "Assistant on the Google Home"
-						        ]
-						    },
-						    title: "Google Home",
-						   
-						},
-						{
-						    optionInfo: {
-						        key: "googlePixel",
-						        synonyms: [
-									"Google Pixel XL",
-									"Pixel", "Pixel XL"
-						        ]
-						    },
-						    title: "Google Pixel",
-						   
-						}
-						
-                    ]
+		    items: obj
                 }
             }
         }
