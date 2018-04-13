af92e51e1c73ae9b68a96a929343e5dfdf5e6c54
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-13 08:19:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 
-
+const App = require('actions-on-google').DialogflowApp;
 
 
 var obj = [];
@@ -24,7 +24,7 @@ restService.use(bodyParser.json());
 
 restService.post("/slack-test", function (req, res) {
 	
-	
+	const app = new App({req, res});
 	
 	var  speech =
       req.body.result &&
