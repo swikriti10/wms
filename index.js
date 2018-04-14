7836259a714b0776509095328906d19eebfd5539
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-04-14 16:22:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,36 +20,38 @@ restService.use(bodyParser.json());
 
 restService.post("/slack-test", function (req, res) {
 
-    const app = new ActionsSdkApp({ request: req, response: res });
-  const NAME_ACTION = 'input.welcome';
-
-    function welcomeIntent(app) {
-        app.askWithList('Which of these looks good?',
-          app.buildList('List title')
-           .addItems([
-             app.buildOptionItem(SELECTION_KEY_ONE,
-               ['synonym of KEY_ONE 1', 'synonym of KEY_ONE 2'])
-               .setTitle('Number one'),
-             app.buildOptionItem(SELECTION_KEY_TWO,
-               ['synonym of KEY_TWO 1', 'synonym of KEY_TWO 2'])
-               .setTitle('Number two'),
-           ]));
-    }
-
-    function optionIntent(app) {
-        if (app.getSelectedOption() === SELECTION_KEY_ONE) {
-            app.tell('Number one is a great choice!');
-        } else {
-            app.tell('Number two is a great choice!');
-        }
-    }
-
-    const actionMap = new Map();
-    actionMap.set(NAME_ACTION, welcomeIntent);
-    //actionMap.set(app.StandardIntents.OPTION, optionIntent);
-    app.handleRequest(actionMap);
-});
-
+    
+ function list () {
+ // const app = new ActionsSdkApp({request, response});
+   const app = new ActionsSdkApp({ request: req, response: res });
+  app.askWithList('Alright! Here are a few things you can learn. Which sounds interesting?',
+    // Build a list
+    app.buildList('Things to learn about')
+      // Add the first item to the list
+      .addItems(app.buildOptionItem('MATH_AND_PRIME',
+        ['math', 'math and prime', 'prime numbers', 'prime'])
+        .setTitle('Math & prime numbers')
+        .setDescription('42 is an abundant number because the sum of its ' +
+        'proper divisors 54 is greater…')
+        .setImage('http://example.com/math_and_prime.jpg', 'Math & prime numbers'))
+      // Add the second item to the list
+      .addItems(app.buildOptionItem('EGYPT',
+        ['religion', 'egpyt', 'ancient egyptian'])
+        .setTitle('Ancient Egyptian religion')
+        .setDescription('42 gods who ruled on the fate of the dead in the ' +
+        'afterworld. Throughout the under…')
+        .setImage('http://example.com/egypt', 'Egypt')
+      )
+      // Add third item to the list
+      .addItems(app.buildOptionItem('RECIPES',
+        ['recipes', 'recipe', '42 recipes'])
+        .setTitle('42 recipes with 42 ingredients')
+        .setDescription('Here\'s a beautifully simple recipe that\'s full ' +
+        'of flavor! All you need is some ginger and…')
+        .setImage('http://example.com/recipe', 'Recipe')
+      )
+  );
+}
 
 
 restService.listen(process.env.PORT || 8000, function () {
